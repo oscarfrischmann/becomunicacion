2ff8c8e8cc9f3cd7fd66b133a347e665a7fd7ba2
Oscar Frischmann <oscarfrisc@gmail.com>
2023-10-09 16:53:28 -0300

    js for dropdownContainer bug  DONE

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-console.log('first');
+const dropDownContainer = document.getElementById('navbarNavDropdown');
+
+const dropDownItem = document.querySelectorAll('.dropdown-item');
+
+console.log(dropDownItem);
+
+dropDownItem.forEach((e) => {
+	e.addEventListener('click', () => {
+		dropDownContainer.classList.remove('show');
+	});
+});
