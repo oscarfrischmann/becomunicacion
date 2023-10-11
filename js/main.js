db490a21a26c271415d27023721abf2e7cb30515
Oscar Frischmann <oscarfrisc@gmail.com>
2023-10-11 15:06:21 -0300

    photos added

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 const dropDownContainer = document.getElementById('navbarNavDropdown');
-
 const dropDownItem = document.querySelectorAll('.dropdown-item');
 
 console.log(dropDownItem);
 
 dropDownItem.forEach((e) => {
+	e.addEventListener('click', () => {
+		dropDownContainer.classList.remove('show');
+	});
+});
+
+const closeNavbar = document.querySelectorAll('.close-navbar');
+console.log(closeNavbar);
+closeNavbar.forEach((e) => {
 	e.addEventListener('click', () => {
 		dropDownContainer.classList.remove('show');
 	});
