ae2746e65129ab40822a583e5a1263dc20473619
Oscar Frischmann <oscarfrisc@gmail.com>
2023-10-12 09:57:21 -0300

    animation logo-simple

--- js/main.js
+++ js/main.js
@@ -15,3 +15,27 @@ function closeNavbar(e) {
 		dropDownContainer.classList.remove('show');
 	});
 }
+
+//*LOGO simple animation
+setTimeout(() => {
+	logoSimple.classList.remove('animate__animated');
+	logoSimple.classList.toggle('animate__bounceInDown');
+}, 1000);
+const logoSimple = document.querySelector('.logo-simple');
+logoSimple.addEventListener('click', () => {
+	logoSimple.classList.toggle('animate__animated');
+	logoSimple.classList.toggle('animate__bounceInDown');
+	setTimeout(() => {
+		logoSimple.classList.remove('animate__animated');
+		logoSimple.classList.toggle('animate__bounceInDown');
+	}, 1000);
+});
+
+(function getResolution() {
+	console.log(`
+	Hola Aniela!!!
+	
+	  Ancho: ${window.screen.width}
+	  Alto: ${window.screen.height}
+	`);
+})();
