0d96afeeeee7d605f0796863ee95fd824273d31d
Oscar Frischmann <oscarfrisc@gmail.com>
2023-10-27 17:58:53 -0300

    form trials

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js';
-import { getAuth } from 'https://www.gstatic.com/firebasejs/10.5.0/firebase-auth.js';
+import {
+	getAuth,
+	onAuthStateChanged,
+	GoogleAuthProvider,
+	signInWithPopup,
+	signOut,
+} from 'https://www.gstatic.com/firebasejs/10.5.0/firebase-auth.js';
 import {
 	getFirestore,
 	collection,
 	addDoc,
 	setDoc,
 	doc,
+	getDocs,
+	deleteDoc,
 } from 'https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore.js';
 
 const dropDownContainer = document.getElementById('navbarNavDropdown');
@@ -167,6 +175,8 @@ const firebaseConfig = initializeApp({
 	messagingSenderId: '499964412994',
 	appId: '1:499964412994:web:16f832a42443b0751159db',
 });
+
+//*databes write
 const db = getFirestore(firebaseConfig);
 
 const form = document.getElementById('contactForm');
@@ -196,4 +206,139 @@ function submitForm(e) {
 		}
 	})();
 }
-form.addEventListener('submit', submitForm);
+if (window.location.href.includes('login')) {
+	console.log('');
+} else {
+	form.addEventListener('submit', submitForm);
+	console.log('no');
+}
+
+//* google auth
+
+const auth = getAuth(firebaseConfig);
+const provider = new GoogleAuthProvider();
+
+//* SIGN IN GOOGLE
+const signInButton = document.getElementById('googleLogIn');
+signInButton.addEventListener('click', () => {
+	signInWithPopup(auth, provider)
+		.then((result) => {
+			// This gives you a Google Access Token. You can use it to access the Google API.
+			const credential = GoogleAuthProvider.credentialFromResult(result);
+			const token = credential.accessToken;
+			// The signed-in user info.
+			const user = result.user;
+			console.log(result);
+			const userID = { userID: user.uid };
+			console.log(userID);
+			(async function () {
+				try {
+					await setDoc(doc(db, 'Users', user.displayName), userID).then(
+						console.log('User ID added')
+					);
+				} catch (e) {
+					console.error('User ID NOT added: ', e);
+				}
+			})();
+			// IdP data available using getAdditionalUserInfo(result)
+			// ...
+			// console.log(getMessages(db));
+		})
+		.catch((error) => {
+			// Handle Errors here.
+			const errorCode = error.code;
+			const errorMessage = error.message;
+			// The email of the user's account used.
+			const email = error.customData.email;
+			// The AuthCredential type that was used.
+			const credential = GoogleAuthProvider.credentialFromError(error);
+			// ...
+		});
+});
+
+//sign out
+const signOutButton = document.getElementById('googleLogOut');
+signOutButton.addEventListener('click', () => {
+	signOut(auth)
+		.then(() => {
+			console.log('Signed Out succesfully');
+		})
+		.catch((error) => {
+			console.log('We couldn´t sign you Out', error);
+		});
+});
+
+// detect auth state
+onAuthStateChanged(auth, (user) => {
+	if (user != null) {
+		console.log('User Logged In');
+		console.log(user);
+		getMessages(db);
+	} else {
+		console.log('No User Logged In');
+	}
+});
+
+//GET INFO
+const msgContainer = document.getElementById('messages');
+let allMessagesInDB;
+async function getMessages(db) {
+	try {
+		const mensajes = collection(db, 'contacto');
+		const mensajesSnapshot = await getDocs(mensajes);
+		console.log(mensajesSnapshot);
+		allMessagesInDB = mensajesSnapshot.docs.map((doc) => doc.data());
+		const withID = mensajesSnapshot.docs.map((doc) => doc.id);
+		console.log(withID);
+		console.log(allMessagesInDB);
+
+		allMessagesInDB.forEach((msg, i) => {
+			msg.id = withID[i];
+			let newMsgDiv = document.createElement('div');
+			newMsgDiv.className = 'msgContainer';
+
+			const email = document.createElement('span');
+			email.textContent = ` ${msg.email}`;
+
+			const text = document.createElement('span');
+			text.textContent = ` ${msg.text}`;
+
+			msgContainer.appendChild(newMsgDiv);
+			newMsgDiv.appendChild(email);
+			newMsgDiv.appendChild(text);
+
+			const deleteMsg = document.createElement('button');
+			deleteMsg.innerHTML = 'Borrar Mensaje';
+			deleteMsg.setAttribute('data-id', msg.id);
+			newMsgDiv.appendChild(deleteMsg);
+		});
+		const deleteBTNS = msgContainer.querySelectorAll('#messages button ');
+		console.log(deleteBTNS);
+		for (let btn of deleteBTNS) {
+			btn.addEventListener('click', async ({ target: { dataset } }) => {
+				deleteMsg(dataset.id);
+				setTimeout(() => {
+					location.reload();
+				}, 1000);
+			});
+		}
+
+		return allMessagesInDB;
+	} catch (e) {
+		console.error('Error getting documents', e);
+	}
+}
+// console.log(getMessages(db));
+const getData = document.getElementById('getData');
+
+getData.addEventListener('click', () => {
+	location.reload();
+	// getMessages(db);
+	// 	setTimeout(() => {
+
+	// 	}, 3000);
+});
+
+function deleteMsg(tg) {
+	deleteDoc(doc(db, 'contacto', tg));
+}
